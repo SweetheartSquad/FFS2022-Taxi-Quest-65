6162d1b4c23f099324a6e93e7bf74af4fb2911d0
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-01-28 00:41:50 -0500

    start looking out window

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -105,9 +105,8 @@ export class GameScene extends GameObject {
 		this.pointDialogue.visible = false;
 
 		this.camera3d = Camera3D.main;
-
-		this.x = 0;
-		this.y = 10;
+		this.x = 44;
+		this.y = -7;
 		this.scripts.push(
 			new Updater(this, () => {
 				const input = getInput();
